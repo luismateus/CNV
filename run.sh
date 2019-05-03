082946c1f10e9bc0df198f56fe0e72b9e5ca2f22
luismateus <luis.afonso.mateus@gmail.com>
2019-05-03 19:24:02 +0100

    fixed script

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#PATH=/home/hypz/Documents/workspace/CNV/CNV-proj
+PATH=/home/ec2-user/CNV-proj
 
-
-rm pt/ulisboa/tecnico/cnv/server/WebServer.class
-javac pt/ulisboa/tecnico/cnv/server/WebServer.java
-java  pt/ulisboa/tecnico/cnv/server/WebServer
+cd $PATH
+/bin/rm pt/ulisboa/tecnico/cnv/server/WebServer.class
+/usr/bin/javac pt/ulisboa/tecnico/cnv/server/WebServer.java
+/usr/bin/java pt/ulisboa/tecnico/cnv/server/WebServer
