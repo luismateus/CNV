137a0bc4b7f6a38af01b6129e72cf361696f21d4
João Robalo <joao.tomas.robalo@gmail.com>
2019-05-17 17:54:14 +0100

    Logica para CPU Usage

--- ScallingTask.java
+++ ScallingTask.java
@@ -10,24 +10,68 @@ public class ScallingTask extends TimerTask {
     private int running;
     private int pending;
     private ArrayList<Instance> runningInstances;
-    
+    private ArrayList<Instance> occupiedInstances;
+    private float MAX_CPU_USAGE = 100;
+    private float MAX_CPU_USAGE_BEFORE_LAUNCH = 0.75f;
+    private float MIN_CPU_USAGE_BEFORE_TERMINATE = 0.25f;
 
     // Add your task here
 	public void run() {
         System.out.println("SCALLING! :)");
-        updateMetrics();
+        update();
         decide();
     }
 
-    public void updateMetrics(){
-        // READ FROM DB
+    public void update(){
         ArrayList<Instance> runningInstances = AutoScaler.getAutoScaler().getInstances();
+        ArrayList<Instance> occupiedInstances = LoadBalancer.getLoadBalancer().getOccupiedInstances();
         this.running = runningInstances.size();
         this.pending = AutoScaler.getAutoScaler().getPendingInstances();
     }
 
     public void decide(){
-        if(running == 0 && pending == 0)
+        if(this.running == 0 && this.pending == 0){
             AutoScaler.getAutoScaler().launch();
+            return;
+        }
+        if(this.pending != 0 && this.running == 1)
+            return;
+        if(getSystemCPUUsage() > MAX_CPU_USAGE * MAX_CPU_USAGE_BEFORE_LAUNCH){
+            AutoScaler.getAutoScaler().launch();
+            return;
+        }
+        if(getSystemCPUUsage() < MAX_CPU_USAGE * MIN_CPU_USAGE_BEFORE_TERMINATE){
+            terminateAnInstance();
+            return;
+        }
+    }
+
+    public float getSystemCPUUsage(){
+        return AutoScaler.getAutoScaler().getSystemCPUUsage();
+    }
+
+    public float getInstanceCPUUsage(Instance instance){
+        return AutoScaler.getAutoScaler().getInstanceCPUUsage(instance);
+    }
+
+    public void terminateAnInstance(){
+        boolean found;
+        for(Instance instanceR : runningInstances){
+            found = false;
+            for(Instance instanceO : occupiedInstances){
+                if((instanceR.getInstanceId()).equals(instanceO.getInstanceId())){
+                    found = true;
+                    break;
+                }
+            }
+            if(found){
+                continue;
+            }
+            else{
+                AutoScaler.getAutoScaler().terminate(instanceR);
+                return;
+            }
+        }
+        AutoScaler.getAutoScaler().setTerminateNextFlag(true);
     }
 }
